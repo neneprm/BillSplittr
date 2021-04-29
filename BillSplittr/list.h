f1bf47b1dae5a0c4b6076645373516ae3ff47ace
neneprm <s.prima@hotmail.com>
2021-04-30 02:38:11 +0700

    Create List class
    list.cpp
    list.h

---
+++ BillSplittr/list.h
@@ -0,0 +1,16 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include "payer.h"
+#include <QString>
+
+class List
+{
+private:
+    QString listName;
+    int price;
+public:
+    List(QString listName = "", int price = 0);
+};
+
+#endif // LIST_H
