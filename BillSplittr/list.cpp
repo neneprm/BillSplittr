b985a93a857dde9479365e9432a5ed70937f2cc4
neneprm <s.prima@hotmail.com>
2021-05-03 01:09:43 +0700

    Update list function
    Set list name, price, price per person
    Set new price
    Set new per person

--- BillSplittr/list.cpp
+++ BillSplittr/list.cpp
@@ -5,4 +5,23 @@ List::List(QString listName, int price,int perPerson)
     this->listName = listName;
     this->price = price;
     this->perPerson = perPerson;
+
+    listLabel = new QLabel();
+    priceLabel = new QLabel();
+    perPersonLabel = new QLabel();
+    listLabel->setText(listName);
+    priceLabel->setText(QString::number(price));
+    perPersonLabel->setText(QString::number(perPerson));
+}
+
+void List::setNewPrice(int newPrice)
+{
+    this->price = newPrice;
+    this->priceLabel->setText(QString::number(newPrice));
+}
+
+void List::setNewPerPerson(int newPerPerson)
+{
+    this->perPerson = newPerPerson;
+    this->perPersonLabel->setText(QString::number(newPerPerson));
 }
--- BillSplittr/list.h
+++ BillSplittr/list.h
@@ -13,6 +13,13 @@ private:
 
 public:
     List(QString listName = "", int price = 0,int perPerson = 0);
+
+    QLabel *listLabel;
+    QLabel *priceLabel;
+    QLabel *perPersonLabel;
+
+    void setNewPrice(int newPrice);
+    void setNewPerPerson(int newPerPerson);
 };
 
 #endif // LIST_H
